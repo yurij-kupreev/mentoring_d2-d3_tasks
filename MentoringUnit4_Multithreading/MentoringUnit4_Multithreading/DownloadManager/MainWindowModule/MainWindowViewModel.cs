a73ea25260ac4b49c08cf1821d5d9c3f4470d261
Yury_Kupreyeu <yurij.kupreev@gmail.com>
2017-10-21 17:29:32 +0300

    Multithreading task completed

---
+++ MentoringUnit4_Multithreading/MentoringUnit4_Multithreading/DownloadManager/MainWindowModule/MainWindowViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Windows;
+using DownloadManager.Helpers;
+using DownloadManager.Services;
+
+namespace DownloadManager.MainWindowModule
+{
+  public class MainWindowViewModel : INotifyPropertyChanged
+  {
+    private readonly DownloadService _downloadService = new DownloadService();
+    private CancellationTokenSource _cancellationTokenSource;
+
+    private string _sourceAddress;
+    public string SourceAddress
+    {
+      get { return _sourceAddress; }
+      set
+      {
+        _sourceAddress = value;
+        OnPropertyChanged();
+      }
+    }
+
+    private string _content;
+    public string Content
+    {
+      get { return _content; }
+      set
+      {
+        _content = value;
+        OnPropertyChanged();
+      }
+    }
+
+    private bool _isDownloadingInProgress;
+    public bool IsDownloadingInProgress
+    {
+      get { return _isDownloadingInProgress; }
+      set
+      {
+        _isDownloadingInProgress = value;
+        OnPropertyChanged();
+      }
+    }
+
+    private RelayCommand _cancelCommand;
+    public RelayCommand CancelCommand => _cancelCommand ?? (_cancelCommand = new RelayCommand(obj => _cancellationTokenSource?.Cancel()));
+
+    private RelayCommand _downloadCommand;
+    public RelayCommand DownloadCommand => _downloadCommand ?? (_downloadCommand = new RelayCommand(this.DownloadCommandAction));
+
+    public async void DownloadCommandAction(object obj)
+    {
+      IsDownloadingInProgress = true;
+      _cancellationTokenSource = new CancellationTokenSource();
+      this.Content = "Loading...";
+
+      this.Content = await _downloadService.Download(this.SourceAddress, _cancellationTokenSource.Token);
+
+      IsDownloadingInProgress = false;
+
+      MessageBox.Show("Operation has been completed.", "Done.", MessageBoxButton.OK);
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName]string prop = "")
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+  }
+}
---
+++ MentoringUnit4_Multithreading/MentoringUnit4_Multithreading/DownloadManager/Services/DownloadService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DownloadManager.Services
+{
+  public class DownloadService
+  {
+    public async Task<string> Download(string address, CancellationToken token)
+    {
+      using (var client = new HttpClient())
+      {
+        string html;
+
+        try
+        {
+          var responseMessage = await client.GetAsync(address, token);
+          html = await responseMessage.Content.ReadAsStringAsync();
+        }
+        catch (TaskCanceledException)
+        {
+          html = "Downloading has been cancelled.";
+        }
+
+        return html;
+      }
+    }
+  }
+}
