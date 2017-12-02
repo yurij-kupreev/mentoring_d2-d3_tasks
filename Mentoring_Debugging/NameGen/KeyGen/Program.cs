7bde5ce071fcb9ac902d8310fc266c241e44a087
Yury_Kupreyeu <yurij.kupreev@gmail.com>
2017-12-02 23:16:25 +0300

    added debugging task (keygen)

---
+++ Mentoring_Debugging/NameGen/KeyGen/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace KeyGen
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var addressBytes = NetworkInterface.GetAllNetworkInterfaces().First().GetPhysicalAddress().GetAddressBytes();
+
+            var dateBytes = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+
+            var array = addressBytes.Select((byteValue, index) => byteValue ^ dateBytes[index]).Select(value =>
+            {
+                if (value <= 999) {
+                    return value * 10;
+                }
+                return value;
+            }).ToArray();
+
+            var mainKey = array.Select(item => Convert.ToString(item));
+
+            var key = string.Join("-", mainKey);
+
+            Console.WriteLine(key);
+        }
+    }
+}
---
+++ Mentoring_Debugging/NameGen/NameGen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrackMe;
+
+namespace NameGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var form = new Form1();
+
+            //assembly has been recompiled to make all its methods public
+            form.eval_a(new List<string> { "123", "234"}.ToArray());
+
+            form.eval_a();
+        }
+    }
+}
